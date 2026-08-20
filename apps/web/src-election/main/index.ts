bf0e9a7c0975d0ed2badc83c10fd92f04f88a56c
shixiaoze <2385614985@qq.com>
2026-08-20 10:52:46 +0800

    fix(electron): enable desktop window shadow

--- apps/web/src-election/main/index.ts
+++ apps/web/src-election/main/index.ts
@@ -1291,7 +1291,7 @@ const getWindowConfig = () => {
     // frame: true, // * app边框(包括关闭,全屏,最小化按钮的导航栏) @false: 隐藏
     // titleBarStyle: "hidden",
     // transparent: true, // * app 背景透明
-    hasShadow: false, // * app 边框阴影
+    hasShadow: true, // * app 边框阴影
     show: false, // 启动窗口时隐藏,直到渲染进程加载完成「ready-to-show 监听事件」 再显示窗口,防止加载时闪烁
     resizable: true, // 禁止手动修改窗口尺寸
     // Windows: 允许用户按 Alt 键显示/隐藏菜单栏
